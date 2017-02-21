105e54ec2b1fdfeaf9034aac26afaf5912c767d3
Sunghyon Kyeong <sunghyon.kyeong@gmail.com>
2017-02-22 04:07:33 +0900

    dddd

--- utils/reslice_img.m
+++
@@ -1,8 +0,0 @@
-function reslice_img(vref, image_to_reslice)
-
-source = image_to_reslice;
-target = vref;
-fns = char(target,source);
-
-flags = struct('interp',0,'mask',0,'mean',0,'which',1,'wrap',[0 0 0]');
-spm_reslice(fns,flags);
